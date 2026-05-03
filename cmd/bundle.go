e651bd97272229d12250499b9bad9305adb3902e
sa1l <sa1l.1024@outlook.com>
2026-05-03 18:06:15 +0800

    feat(cmd): add clipboard support to bundle command for easier output management

--- cmd/bundle.go
+++ cmd/bundle.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"runtime"
+	"strings"
 
 	"github.com/sa1la/sinit/utils/atcoder"
 	"github.com/sa1la/sinit/utils/runner"
@@ -15,6 +17,7 @@ func init() {
 	var problem string
 	var contestID string
 	var output string
+	var copyFlag bool
 
 	bundleCmd := &cobra.Command{
 		Use:   "bundle",
@@ -77,18 +80,25 @@ func init() {
 				return
 			}
 
+			data, err := os.ReadFile(bundlePath)
+			if err != nil {
+				fmt.Println("Error reading bundle:", err)
+				return
+			}
+
 			if output != "" {
-				if err := os.Rename(bundlePath, output); err != nil {
+				if err := os.WriteFile(output, data, 0644); err != nil {
 					fmt.Println("Error writing output:", err)
 					return
 				}
 				fmt.Printf("Bundle written to %s\n", output)
-			} else {
-				data, err := os.ReadFile(bundlePath)
-				if err != nil {
-					fmt.Println("Error reading bundle:", err)
+			} else if copyFlag {
+				if err := copyToClipboard(string(data)); err != nil {
+					fmt.Println("Error copying to clipboard:", err)
 					return
 				}
+				fmt.Println("Bundle copied to clipboard")
+			} else {
 				fmt.Print(string(data))
 			}
 		},
@@ -97,6 +107,27 @@ func init() {
 	bundleCmd.Flags().StringVarP(&problem, "problem", "p", "", "problem ID (e.g., c, 455c, abc455c)")
 	bundleCmd.Flags().StringVarP(&contestID, "contest", "c", "", "contest ID (defaults to current directory name)")
 	bundleCmd.Flags().StringVarP(&output, "output", "o", "", "output file (default: stdout)")
+	bundleCmd.Flags().BoolVar(&copyFlag, "copy", false, "copy bundle to clipboard")
 
 	rootCmd.AddCommand(bundleCmd)
+}
+
+func copyToClipboard(text string) error {
+	var cmd *exec.Cmd
+	switch runtime.GOOS {
+	case "darwin":
+		cmd = exec.Command("pbcopy")
+	case "windows":
+		cmd = exec.Command("clip")
+	default:
+		if _, err := exec.LookPath("xclip"); err == nil {
+			cmd = exec.Command("xclip", "-selection", "clipboard", "-in")
+		} else if _, err := exec.LookPath("xsel"); err == nil {
+			cmd = exec.Command("xsel", "--clipboard", "--input")
+		} else {
+			return fmt.Errorf("no clipboard utility found (install xclip or xsel)")
+		}
+	}
+	cmd.Stdin = strings.NewReader(text)
+	return cmd.Run()
 }
